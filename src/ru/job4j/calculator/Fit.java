1c1f3f897fda134cc5861f979f19bcd5b12a0e9c
Insomn1ac <emelyanoff94@gmail.com>
2021-04-24 23:47:08 +0300

    Идеальный вес [#525421]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(int height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(int height) {
+        return (height - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        double man = Fit.manWeight(182);
+        double woman = Fit.womanWeight(167);
+        System.out.println("For man, 182 tall, ideal weight is " + man);
+        System.out.println("For woman, 167 tall, ideal weight is " + woman);
+    }
+}
